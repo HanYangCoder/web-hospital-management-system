06ab5f2246da5bd8065c8003e307e0ec237e6eba
root <root@pop-os.localdomain>
2021-02-09 21:34:37 +0800

    Added index and validateLogin, indented files, added testFolder

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Online Hosp Login</title>    
+  </head>
+
+  <body>
+
+    <form name="login_old_member" action="index.php" method="GET">
+      <div>
+        <input type="text" id = "username" name = "username" class="form-control" placeholder="Enter Username" required="true"/>
+      </div>
+
+      <div>
+        <input type="password" id = "password" name = "password" class="form-control" placeholder="Enter Password" required="true"/>
+      </div>
+    </form>
+
+    <div>
+      <input type="submit" value="LOG IN" id="submit" name="login_old_member"/>
+    </div>
+
+  </body>
+</html>
