35ef806608f5877c26c66760f2fed6d9e63545f1
root <root@pop-os.localdomain>
2021-02-11 21:38:48 +0800

    showAll and showOnly working, need links and forms to update DBs

--- showAll.php
+++ showAll.php
@@ -1,33 +1,46 @@
 <?php
   //echo"Hello World! I am $username and I showAll";
-  require  ('db_connnection.php');
+  require ('db_connection.php');
 
   //TODO: showUserDbSql, showDoctorDbSql, showHospitalDbSql
   $showUserDbSql = $conn->prepare ("Select * from userDB");
   $showUserDbSql->execute();
   $showUserDb = $showUserDbSql->fetchAll();
+
+  $showHospitalDbSql = $conn->prepare ("Select * from hospitalDB");
+  $showHospitalDbSql->execute();
+  $showHospitalDb = $showHospitalDbSql->fetchAll();
+
+  $showDoctorDbSql = $conn->prepare ("Select * from doctorDB");
+  $showDoctorDbSql->execute();
+  $showDoctorDb = $showDoctorDbSql->fetchAll();
 ?>
 
 <!DOCTYPE html>
 <html>
 
   <head>
-    
+    <style>
+      table, th,td {
+        border: 1px solid black;
+      }
+    </style>
   </head>
 
   <body>
 
+    <!--userDB table-->
     <table>
       <thead>
         <tr>
 
-          <th><p> User ID <p><th>
-          <th><p> Username <p><th>
-          <th><p> First name <p><th>
-          <th><p> Last name <p><th>
-          <th><p> Password <p><th>
-          <th><p> Email <p><th>
-          <th><p> User role <p><th>
+          <th><p> User ID <p></th>
+          <th><p> Username <p></th>
+          <th><p> First name <p></th>
+          <th><p> Last name <p></th>
+          <th><p> Password <p></th>
+          <th><p> Email <p></th>
+          <th><p> User role <p></th>
         
         </tr>
       </thead>
@@ -44,6 +57,72 @@
           <td><p> <?= $UserList['password']; ?> </p></td>
           <td><p> <?= $UserList['email']; ?> </p></td>
           <td><p> <?= $UserList['userrole']; ?> </p></td>
+
+        </tr>
+      </tbody>
+
+      <?php endforeach; ?>
+    </table>
+
+    <br>
+
+    <!--hospitalDB table-->
+    <table>
+      <thead>
+        <tr>
+
+          <th><p> Room Number <p></th>
+          <th><p> Patient Name <p></th>
+          <th><p> Doctor in charge <p></th>
+        
+        </tr>
+      </thead>
+
+      <?php foreach ($showHospitalDb as $HospitalList): ?>
+
+      <tbody>
+        <tr>
+
+          <td><p> <?= $HospitalList['roomNum']; ?> </p></td>
+          <td><p> <?= $HospitalList['patientName']; ?> </p></td>
+          <td><p> <?= $HospitalList['docInCharge']; ?> </p></td>
+
+        </tr>
+      </tbody>
+
+      <?php endforeach; ?>
+    </table>
+
+    <br>
+
+    <!--doctorDB table-->
+    <table>
+      <thead>
+        <tr>
+
+          <th><p> Doctor ID <p></th>
+          <th><p> First name <p></th>
+          <th><p> Last name <p></th>
+          <th><p> Department <p></th>
+          <th><p> Specialty <p></th>
+          <th><p> Contact number <p></th>
+          <th><p> Is Available <p></th>
+        
+        </tr>
+      </thead>
+
+      <?php foreach ($showDoctorDb as $DoctorList): ?>
+
+      <tbody>
+        <tr>
+
+          <td><p> <?= $DoctorList['doctorID']; ?> </p></td>
+          <td><p> <?= $DoctorList['firstName']; ?> </p></td>
+          <td><p> <?= $DoctorList['lastName']; ?> </p></td>
+          <td><p> <?= $DoctorList['department']; ?> </p></td>
+          <td><p> <?= $DoctorList['specialty']; ?> </p></td>
+          <td><p> <?= $DoctorList['contactNum']; ?> </p></td>
+          <td><p> <?= $DoctorList['isAvailable']; ?> </p></td>
 
         </tr>
       </tbody>
--- showOnly.php
+++ showOnly.php
@@ -1,3 +1,92 @@
 <?php
-  echo"Hello World! I am $username and I showOnly";
+  require ('db_connection.php');
+
+  //TODO: showDoctorDbSql, showHospitalDbSql
+  $showHospitalDbSql = $conn->prepare ("Select * from hospitalDB");
+  $showHospitalDbSql->execute();
+  $showHospitalDb = $showHospitalDbSql->fetchAll();
+
+  $showDoctorDbSql = $conn->prepare ("Select * from doctorDB");
+  $showDoctorDbSql->execute();
+  $showDoctorDb = $showDoctorDbSql->fetchAll();
 ?>
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <style>
+      table, th,td {
+        border: 1px solid black;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <!--hospitalDB table-->
+    <table>
+      <thead>
+        <tr>
+
+          <th><p> Room Number <p></th>
+          <th><p> Patient Name <p></th>
+          <th><p> Doctor in charge <p></th>
+        
+        </tr>
+      </thead>
+
+      <?php foreach ($showHospitalDb as $HospitalList): ?>
+
+      <tbody>
+        <tr>
+
+          <td><p> <?= $HospitalList['roomNum']; ?> </p></td>
+          <td><p> <?= $HospitalList['patientName']; ?> </p></td>
+          <td><p> <?= $HospitalList['docInCharge']; ?> </p></td>
+
+        </tr>
+      </tbody>
+
+      <?php endforeach; ?>
+    </table>
+
+    <br>
+
+    <!--doctorDB table-->
+    <table>
+      <thead>
+        <tr>
+
+          <th><p> Doctor ID <p></th>
+          <th><p> First name <p></th>
+          <th><p> Last name <p></th>
+          <th><p> Department <p></th>
+          <th><p> Specialty <p></th>
+          <th><p> Contact number <p></th>
+          <th><p> Is Available <p></th>
+        
+        </tr>
+      </thead>
+
+      <?php foreach ($showDoctorDb as $DoctorList): ?>
+
+      <tbody>
+        <tr>
+
+          <td><p> <?= $DoctorList['doctorID']; ?> </p></td>
+          <td><p> <?= $DoctorList['firstName']; ?> </p></td>
+          <td><p> <?= $DoctorList['lastName']; ?> </p></td>
+          <td><p> <?= $DoctorList['department']; ?> </p></td>
+          <td><p> <?= $DoctorList['specialty']; ?> </p></td>
+          <td><p> <?= $DoctorList['contactNum']; ?> </p></td>
+          <td><p> <?= $DoctorList['isAvailable']; ?> </p></td>
+
+        </tr>
+      </tbody>
+
+      <?php endforeach; ?>
+    </table>
+
+  </body>
+</html>
